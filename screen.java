b13f23d9f985beeab21856b23a5a2a048bc4e662
Jon <jposey79@gmail.com>
2013-11-05 15:16:37 -0800

    updated screen.java. still needs to be put together.

--- screen.java
+++ screen.java
@@ -1,28 +1,51 @@
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JButton;
+import javax.swing.*;
+import java.awt.event.*;
 
 public class screen extends JFrame {
-		
+
+    private JPanel panel,panel1;
+    private JButton button;
+    private JLabel label;
+    private JTextField server_id,username_id,password_id,extention_id;
+
+    private int totalClicked;
+
     	public void screen(){
 
+	    	totalClicked = 0;
+
 	    	//setup JFrame
-		JFrame jf = new JFrame();
-		setVisible(true);
-		setTitle("Polycom Phone Dialer");
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setSize(400,400);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLocationRelativeTo(null);
 
-		//setup JLabel
-	    	JLabel jl = new JLabel();
-		setSize(850,250);
+		panel = new JPanel();
+	
+		button = new JButton("Enter");
 
-		//set up buttons
-		JButton enterBtn = new JButton("Enter");
-		setVisible(true);
+		server_id = new JTextField(10);
+		extention_id = new JTextField(10);
+		username_id = new JTextField(10);
+		password_id = new JTextField(10);
 
-		//adding elements to window
-		add(jl);
-		add(enterBtn);
+		label = new JLabel(" ");
+		
+		button.addActionListener(new ActionListener(){
+		    
+		    public void actionPerformed(ActionEvent e){
+			label.setText("enter is clicked " + totalClicked);
+			totalClicked++;
+			}
+		});
+		
+		panel.add(button);
+		panel.add(label);
+		panel.add(server_id);
+		panel.add(extention_id);
+		panel.add(username_id);
+		panel.add(password_id);
+
+		this.add(panel);
+		this.setVisible(true);
 	}	
-
 }
